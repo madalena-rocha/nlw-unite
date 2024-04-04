1065181627b3e885f33672a7bcf9fd8c6b8c944d
madalena-rocha <rochamada1997m@gmail.com>
2024-04-03 21:32:41 -0300

    font configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,32 @@
 import "../styles/global.css"
 
 import { Slot } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function Layout() {
-  // O Slot repassa toda a estrutura de navegação da aplicação
-  return <Slot />
+  // As fontes são carregadas de forma assíncrona, sendo necessário garantir que as fontes sejam carregadas no dispositivo antes do app abrir
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+  
+  return (
+    <>
+      <StatusBar style="light" />
+      {/* O Slot repassa toda a estrutura de navegação da aplicação */}
+      <Slot />
+    </>
+  )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     // No React Native, o flexbox já é ativo por padrão
     <View className="flex-1 bg-green-500 items-center justify-center">
-      <Text className="text-white">Hello React Native!</Text>
+      <Text className="text-white text-2xl">Hello React Native!</Text>
     </View>
   )
 }
